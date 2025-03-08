843dae585926c17dcb8c38af06f507fdf7f01f6e
Mohammed Kashif <kashiftechy847@gmail.com>
2025-03-08 08:10:00 +0530

    Added a new feature

---
+++ second_highest.py
@@ -0,0 +1,11 @@
+li = [3,0,2,6,4,-5,-1,7,9,-8]
+
+first = float('-inf')
+second = float('-inf')
+for i in li:
+    if i>first:
+        second = first 
+        first = i
+    elif i>second and i != first:
+        second = i
+print(second)
